731cd8a2ae8ea84b0d4c4b33e93a1f7c1c898d33
Shri <shriyashmpatil@gmail.com>
2023-02-08 16:14:17 +0530

    first commit~

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
 function App() {
+  // const [data,setData] = useState({});
+
+  fetch(`https://jsonplaceholder.typicode.com/users`).then((result)=>{
+      result.json().then((resp)=>{
+        console.log(resp);
+      })
+    })
+  // useEffect(()=>{
+    
+  // })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Get Api</h1>
+      {/* <p>{data}</p> */}
     </div>
   );
 }
